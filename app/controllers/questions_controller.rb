96d3768b901474d6f5e9f8f48c32b4ca9a81ca49
Beyda Fentanes <beyfenta@gmail.com>
2023-12-11 12:08:39 -0600

    02 check

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+
+def ask
+end
+def answer
+  @answers = ["Great", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+  if params[:question] == "I am going to work"
+    @answers = @answers[0]
+  elsif params[:question].ends_with?("?")
+    @answers = @answers[1]
+  else
+    @answers = @answers[2]
+  end
+end
+end
